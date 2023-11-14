cf6fc5dab8a69abb7c1c36cadafcabd00b41a70e
naveen p <naveenpendekatla0407@gmail.com>
2023-11-14 20:46:27 +0530

    frontend

--- servers.tf
+++ servers.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "frontend" {
-  ami           = ami-03265a0778a880afb
+  ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
@@ -8,11 +8,10 @@ resource "aws_instance" "frontend" {
   }
 }
 
-resource "aws_route53_record" "frontned" {
-  zone_id = Z04520141JRLZY4JX3O8T
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z04520141JRLZY4JX3O8T"
   name    = "frontend-dev.naveendevops1.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
-

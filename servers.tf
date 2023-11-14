50cab1e26a0d5471e1e3199becf4d0ba4275b00a
naveen p <naveenpendekatla0407@gmail.com>
2023-11-14 20:41:01 +0530

    frontend

---
+++ servers.tf
@@ -0,0 +1,18 @@
+
+resource "aws_instance" "frontend" {
+  ami           = ami-03265a0778a880afb
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontned" {
+  zone_id = Z04520141JRLZY4JX3O8T
+  name    = "frontend-dev.naveendevops1.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+

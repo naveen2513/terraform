08322430f255bab3f6e5c3d3ed02089288b48f4f
naveen p <naveenpendekatla0407@gmail.com>
2023-11-15 13:16:24 +0530

    instances

--- servers.tf
+++ servers.tf
@@ -59,7 +59,7 @@ resource "aws_instance" "instances" {
   for_each = var.components
   ami           = data.aws_ami.ami.image_id
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = data.aws_security_group.allow-all.id
+  vpc_security_group_ids = ["data.aws_security_group.allow-all.id"]
 
   tags = {
     Name = each.value["name"]

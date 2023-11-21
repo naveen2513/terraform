23d214f0a703db2f08c64b3cd561c182fe886548
naveen p <naveenpendekatla0407@gmail.com>
2023-11-21 12:19:38 +0530

    shell

--- servers.tf
+++ servers.tf
@@ -23,7 +23,7 @@ resource "null_resource" "provisioner" {
       "rm -rf roboshop-scripting",
       "git clone https://github.com/naveen2513/roboshop-scripting",
       "cd roboshop-scripting",
-      "sudo bash ${each.value["name"]} ${lookup(each.value,"password" "null" )}"
+      "sudo bash ${each.value["name"]} ${lookup(each.value,"password", "null" )}"
     ]
   }
 }

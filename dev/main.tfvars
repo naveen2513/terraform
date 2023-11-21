7e3c6861670b4b6a6c79c45c024dcb1ade39dd84
naveen p <naveenpendekatla0407@gmail.com>
2023-11-21 12:18:41 +0530

    shell

--- dev/main.tfvars
+++ dev/main.tfvars
@@ -26,18 +26,25 @@ components = {
   mysql = {
     name          = "mysql"
     instance_type = "t3.micro"
+    password = "Roboshop@1"
   }
   shipping = {
     name          = "shipping"
     instance_type = "t2.micro"
+    password = "Roboshop@1"
+
   }
   rabbitmq = {
     name          = "rabbitmq"
     instance_type = "t3.micro"
+    password = "roboshop123"
+
   }
   payment = {
     name          = "payment"
     instance_type = "t3.micro"
+    password = "roboshop123"
+
   }
 
 }
--- servers.tf
+++ servers.tf
@@ -1,18 +1,33 @@
 
-
-
-
-
 resource "aws_instance" "instances" {
-  for_each = var.components
-  ami           = data.aws_ami.ami.image_id
-  instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+  for_each               = var.components
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = each.value["name"]
   }
 }
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instances,aws_route53_record.records]
+  for_each = var.components
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "DevOps321"
+      host     = aws_instance.instances[each.value["name"]].private_ip
+    }
+    inline = [
+      "rm -rf roboshop-scripting",
+      "git clone https://github.com/naveen2513/roboshop-scripting",
+      "cd roboshop-scripting",
+      "sudo bash ${each.value["name"]} ${lookup(each.value,"password" "null" )}"
+    ]
+  }
+}
+
 
 resource "aws_route53_record" "records" {
   for_each = var.components
